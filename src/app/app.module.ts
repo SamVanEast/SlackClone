d6890362e59699b79e82f439380ee5da8b9b6a77
Creativity12345 <sebastianm444@gmail.com>
2023-03-08 12:34:45 +0100

    format code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,8 +35,6 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore/'; 
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -75,7 +73,6 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
     AngularFireModule,
     AngularFirestoreModule,
     AngularFireModule.initializeApp(environment.firebase),
-
   ],
   providers: [],
   bootstrap: [AppComponent]
