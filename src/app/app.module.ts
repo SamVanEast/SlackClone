ce321e38e1822009487c1d1cb45e80879cc37f27
Thomas Wagner <t-wagner@web.de>
2023-03-08 11:29:18 +0100

    trying to include badge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,19 +14,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {MatAutocompleteModule} from '@angular/material/autocomplete'; 
-import {MatBadgeModule} from '@angular/material/badge'; 
-import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
-import {MatDatepickerModule} from '@angular/material/datepicker'; 
-import {MatDialogModule} from '@angular/material/dialog'; 
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatMenuModule} from '@angular/material/menu'; 
-import {MatProgressBarModule} from '@angular/material/progress-bar'; 
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
-import {MatSelectModule} from '@angular/material/select'; 
-import {MatSidenavModule} from '@angular/material/sidenav'; 
-import {MatTooltipModule} from '@angular/material/tooltip'; 
-import {MatTreeModule} from '@angular/material/tree'; 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [
@@ -59,7 +59,7 @@ import {MatTreeModule} from '@angular/material/tree';
     MatSidenavModule,
     MatTooltipModule,
     MatTreeModule
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
