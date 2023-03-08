d866845033b24c35c4a51527daeaa8f8a8dd999f
Samuel Haas <Samuel03102001@gmail.com>
2023-03-08 15:10:32 +0100

    delete wrong named component and add new

---
+++ src/app/messages-history/messages-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messages-history',
+  templateUrl: './messages-history.component.html',
+  styleUrls: ['./messages-history.component.scss']
+})
+export class MessagesHistoryComponent {
+
+}
