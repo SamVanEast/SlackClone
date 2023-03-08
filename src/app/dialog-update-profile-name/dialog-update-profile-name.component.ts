fbc67b567e677f59ddac2cb1f7c1317dc39b2f1c
Creativity12345 <sebastianm444@gmail.com>
2023-03-08 17:11:05 +0100

    add new component

---
+++ src/app/dialog-update-profile-name/dialog-update-profile-name.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-update-profile-name',
+  templateUrl: './dialog-update-profile-name.component.html',
+  styleUrls: ['./dialog-update-profile-name.component.scss']
+})
+export class DialogUpdateProfileNameComponent {
+
+}
