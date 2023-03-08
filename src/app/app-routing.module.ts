488d887b3983f29ef1ed26878694ae46cde72e46
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2023-03-08 15:46:28 +0100

    route changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { SlackOverviewComponent } from './slack-overview/slack-overview.component';
 
 const routes: Routes = [
   {path: '', component:  SlackOverviewComponent},
   {path: ':id', component:  SlackOverviewComponent},
-  {path: 'slack/login', component:  SlackOverviewComponent},
+  {path: 'slack/login', component:  LoginComponent},
   {path: 'slack/overview', component:  SlackOverviewComponent},
 ];
 
