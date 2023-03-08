f336c8daf4544bdabc32c22e584ce8b13daabecb
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2023-03-08 17:10:37 +0100

    update login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import {Router} from '@angular/router';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -7,4 +17,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
+  hide = true;
+  newUser = false;
+
+  guestLogin() {
+    this.router.navigateByUrl('/')
+  }
+
+  NewUserOverview() {
+    this.newUser = true;
+  }
+
+  showLogin() {
+    this.newUser = false;
+  }
 }
