327026b921e5e681a6ddf17029cac28564c7bc43
Samuel Haas <Samuel03102001@gmail.com>
2023-03-09 13:04:20 +0100

    Update navbar-left.component.ts
    test

--- src/app/navbar-left/navbar-left.component.ts
+++ src/app/navbar-left/navbar-left.component.ts
@@ -3,7 +3,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { collectionData } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { collection, doc, Firestore, getFirestore } from '@firebase/firestore';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { user } from 'src/models/user';
 
 @Component({
@@ -23,31 +23,39 @@ export class NavbarLeftComponent {
     this.messages = user.messages
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.route.params.subscribe((params) => {
       this.currentUserId = params['id'];
     });
 
 
 
-    const db = getFirestore();
-    const colRef = collection(db, 'todos'); 
-    this.messagesFromFirebase$ = collectionData(colRef);
+    // const db = getFirestore();
+    // const colRef = collection(db, 'todos'); 
+    // this.messagesFromFirebase$ = collectionData(colRef);
 
-    this.messagesFromFirebase$.subscribe((message) => {
-      console.log(message);
-    })
-
-
-    // this.messagesFromFirebase$ = doc(db, "cities", "2l3bcSGs2vZBIc3RODwp");
+    // this.messagesFromFirebase$.subscribe((message) => {
+    //   console.log(message);
+    // })
 
 
+    // const docRef = doc(db, "channels", "REz7mkotTOrDEP60d4YQ");
+    // const myDocRef = this.firestore.doc(`channels/REz7mkotTOrDEP60d4YQ`);
+    // this.messagesFromFirebase$ = from(myDocRef.get());
+    // this.messagesFromFirebase$.subscribe((doc) => {
+    //   const myData = doc.data();
+    //   console.log(doc); 
+    // }, (error) => {
+    //   console.log('Fehler beim Abrufen des Firestore-Dokuments:', error);
+    // });
+    // console.log(myDocRef);
+    console.log(this.messages);
 
-    this.firestore.collection('users').doc(this.currentUserId).valueChanges().subscribe((user: any) => {
-      this.messages = user.messages;
-      // console.log(this.messages);
-    });
 
+    this.loadMessagesFromFirestore();
+
+
+    console.log(this.messages);
 
 
 
@@ -62,5 +70,13 @@ export class NavbarLeftComponent {
 
 
 
+  }
+
+
+  async loadMessagesFromFirestore() {
+    await this.firestore.collection('users').doc(this.currentUserId).valueChanges().subscribe((user: any) => {
+      this.messages = user.messages;
+      // console.log(this.messages);
+    });
   }
 }
