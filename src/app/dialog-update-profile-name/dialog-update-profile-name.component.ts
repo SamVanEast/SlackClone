8a50936156a9dccc208f3a664dc21927aec66ee0
Creativity12345 <sebastianm444@gmail.com>
2023-03-09 09:22:25 +0100

    update user name works

--- src/app/dialog-update-profile-name/dialog-update-profile-name.component.ts
+++ src/app/dialog-update-profile-name/dialog-update-profile-name.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './dialog-update-profile-name.component.html',
   styleUrls: ['./dialog-update-profile-name.component.scss']
 })
-export class DialogUpdateProfileNameComponent{
+export class DialogUpdateProfileNameComponent {
 
   loading = false;
   public firstName;
@@ -22,6 +22,21 @@ export class DialogUpdateProfileNameComponent{
       console.log(user);
       this.firstName = user.userInfos.firstName;
       this.lastName = user.userInfos.lastName;
+    });
+  }
+
+  saveName() {
+    this.loading = true;
+    const userDocRef = this.firestore.collection('users').doc(this.currentUserId);
+    userDocRef.update({
+      'userInfos.firstName': this.firstName,
+      'userInfos.lastName': this.lastName,
+    }).then(() => {
+      this.loading = false;
+      this.dialogRef.close();
+    }).catch((error) => {
+      this.loading = false;
+      console.log('Error updating user document:', error);
     });
   }
 
