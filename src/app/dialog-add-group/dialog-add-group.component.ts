5f04befe002fda53c26919a711e16d3af1b093aa
Samuel Haas <Samuel03102001@gmail.com>
2023-03-10 12:30:32 +0100

    add problem with add groupId to user

--- src/app/dialog-add-group/dialog-add-group.component.ts
+++ src/app/dialog-add-group/dialog-add-group.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { group } from 'src/models/group';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-dialog-add-group',
@@ -9,21 +11,48 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./dialog-add-group.component.scss']
 })
 export class DialogAddGroupComponent {
+  // communicationSections
   currentUserId;
   loading = false;
-  groupName;
+  group;
+  groupsIds = [];
+  // groupName;
 
-  constructor(private route: ActivatedRoute, private firestore: AngularFirestore, private dialogRef: MatDialogRef<DialogAddGroupComponent>) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore, private dialogRef: MatDialogRef<DialogAddGroupComponent>) {
+    this.group = group;
+    this.route.params.subscribe((params) => {
+      this.currentUserId = params['id'];
+    });
+  }
 
   saveGroup() {
-    this.firestore.collection('groups').add({
-      group: '',
-      headline: this.groupName
-    })
-    .then(() => {
-      console.log('Works');
-      this.dialogRef.close();
-    });
+    if (this.group.headline !== '') {
+      this.firestore.collection('groups').add(this.group).then((group) => {
+        console.log(group.id);
+        console.log(this.firestore.collection('users').doc(this.currentUserId).valueChanges());
+
+        this.firestore.collection('users').doc(this.currentUserId).get().then( user => {
+          this.groupsIds = user.messages.groups;
+          this.groupsIds.push(group.id);
+          // console.log(this.groupsIds);
+          this.firestore.collection('users').doc(this.currentUserId).update({
+            'messages.groups': this.groupsIds
+          }).then((user) => {
+            console.log('Hat funktioniert', this.groupsIds);
+          })
+
+        });
+
+
+
+
+
+
+
+
+        this.dialogRef.close();
+      });
+    }
   }
 
   closeDialogGroup() {
