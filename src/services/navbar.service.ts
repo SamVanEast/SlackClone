6ecb3878d06123394daa03da33a8bd60710a40a2
Creativity12345 <sebastianm444@gmail.com>
2023-03-15 14:09:14 +0100

    optimize autoscroll

--- src/services/navbar.service.ts
+++ src/services/navbar.service.ts
@@ -6,10 +6,18 @@ export class NavbarService {
   showNavbarRight: boolean;
   showNavbarLeft: boolean;
   whichProfileShouldLoad = new Subject<any>();
+  autoScroll: boolean;
 
   constructor() { this.showNavbarRight = false; this.showNavbarLeft = true }
 
   openRight() { this.showNavbarRight = true }
   toggleRight() { this.showNavbarRight = !this.showNavbarRight }
   toggleLeft() { this.showNavbarLeft = !this.showNavbarLeft }
+
+  autoScrollOn() {
+    this.autoScroll = true;
+    setTimeout(() => {
+      this.autoScroll = false;
+    }, 300);
+  }
 }
